a25df2dd0722e7b747bed1e750306bc64a69e798
iamdpk <sapkotad64@gmail.com>
2021-01-10 19:33:44 +0545

    My Complete Static Portfolio site

---
+++ script.js
@@ -0,0 +1,24 @@
+// preloader
+var preloader = document.getElementById("loading");
+		function myFunction(){
+			preloader.style.display = 'none';
+		};
+
+// navbar
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+// jquery
+$(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
